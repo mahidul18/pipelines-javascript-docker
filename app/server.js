02f5a26a4b96df4b886886985a19110804923f90
mahidul18 <45544914+mahidul18@users.noreply.github.com>
2020-09-30 10:24:05 -0400

    Update server.js

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Happy Tuesday Everyone\n');
+  res.send('Hello Everyone\n');
 });
 
 app.listen(PORT, HOST);
